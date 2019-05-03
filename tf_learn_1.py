23aae1bc9a9becacba13a4f4811bf322c6d0e3a0
czqsole <czqsole@gmail.com>
2019-05-04 00:40:12 +0800

    test1

--- tf_learn_1.py
+++ tf_learn_1.py
@@ -1,18 +1,39 @@
+# TensorFlow and tf.keras
 import tensorflow as tf
-mnist = tf.keras.datasets.mnist
+from tensorflow import keras
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+print(tf.__version__)
 
-model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images.shape
+
+len(train_labels)
+
+#prepare
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
